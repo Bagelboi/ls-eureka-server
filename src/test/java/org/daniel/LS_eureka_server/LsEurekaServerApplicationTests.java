2a003f68c051e525eaec60411e5a21f166881ddc
Bagelboi <donkeysoda22@gmail.com>
2024-09-21 02:02:12 -0300

    first commit

---
+++ src/test/java/org/daniel/LS_eureka_server/LsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daniel.LS_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
